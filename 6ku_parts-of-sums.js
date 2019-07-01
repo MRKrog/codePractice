919edbdb5ebae546677fbc21b16f3c1ac3ffeac8
Michael Krog <michaelryankrog@gmail.com>
2019-07-01 12:19:59 -0600

    updated problems

--- 6ku_parts-of-sums.js
+++ 6ku_parts-of-sums.js
@@ -30,18 +30,12 @@
 let ls = [0, 1, 3, 6, 10] // >> [20, 20, 19, 16, 10, 0]
 // let ls = [744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]
 // [10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358, 0]
-// let ls = [1, 2, 3, 4, 5, 6]
 
 function partsSums(ls) {
   let arr = [0];
-  // console.log(ls.reverse())
   ls.reverse().forEach(v => {
-    console.log(v)
-    // console.log(arr[arr.length - 1])
-    console.log(arr)
     arr.push(arr[arr.length - 1] + v)
   })
-  console.log(arr)
   return arr.reverse();
 }
 
---
+++ 6kyu_Connect-Four.js
@@ -0,0 +1,141 @@
+// Connect Four its a game where 2 players, each turn, drop a colored disc in one of the columns of a grid. When a player drops a disc, this will occupy the next space available in the column. The winner is the first player that forms a line (vertical, diagonal or horizontal) of 4 discs of its own color.
+//
+// However, in this kata there are the following twists:
+//
+// There's no gravity, so a disc will just be placed in an XY position
+// The space of the game is virtually infinite, so a disc can be placed in any XY position
+// There can be more than 2 players
+// Given a game situation, where some discs has been already been placed, you have to determine a winner, if there's one.
+//
+// Input
+// The input consists in an array of moves made during the game
+//
+// [
+//   {p: 'R', x: 0, y: 0},
+//   {p: 'Y', x: 100, y: 100},
+//   {p: 'R', x: 1, y: 0},
+//   {p: 'Y', x: 99, y: 100},
+//   {p: 'R', x: 2, y: 0},
+//   {p: 'Y', x: 98, y: 100},
+//   {p: 'R', x: 3, y: 0},
+//   {p: 'Y', x: 96, y: 100}
+// ];
+// You can see in this example that the player R won making the line
+//
+//   {x: 0, y: 0}{x: 1, y: 0}{x: 2, y: 0}{x: 3, y: 0}
+// Be aware that the moves ARE NOT ordered by time, this is an example that would fit well for describe a normal match but the moves you will receive will be in a random order
+//
+// Output
+// You must return the player that won as string, in the example above, you would return 'R'
+//
+// If there's no winner, you must return null
+//
+// Constraints
+// As the standard rules, you must check from rows, columns and diagonals
+// If there's a winner, there will be only one
+// You will not receive empty array, nor invalid inputs
+// There will not be duplicates points
+
+
+
+const moves1 = [
+  {p: 'R', x: 0, y: 0},
+  {p: 'Y', x: 100, y: 100},
+  {p: 'R', x: 1, y: 0},
+  {p: 'Y', x: 99, y: 100},
+  {p: 'R', x: 2, y: 0},
+  {p: 'Y', x: 98, y: 100},
+  {p: 'R', x: 3, y: 0},
+  {p: 'Y', x: 96, y: 100}
+];
+
+
+
+// {x: 0, y: 0}{x: 1, y: 0}{x: 2, y: 0}{x: 3, y: 0}
+
+
+const connectFour = moves => {
+  let players = getPlayers(moves)
+  console.log(players)
+
+}
+
+
+const getPlayers = (moves) => {
+  let players = moves.reduce((acc, type) => {
+    if(!acc.includes(type['p'])){
+      acc.push(type['p'])
+    }
+    return acc
+  }, [])
+
+  return players
+}
+
+connectFour(moves1)
+
+
+
+
+
+const moves2 = [
+  {p: 'R', x: 15, y: 15},
+  {p: 'Y', x: 10, y: 10},
+  {p: 'Y', x: 10, y: 11},
+  {p: 'Y', x: 10, y: 12},
+  {p: 'Y', x: 10, y: 13},
+  {p: 'R', x: 15, y: 16},
+  {p: 'R', x: 16, y: 15},
+  {p: 'R', x: 16, y: 16}
+];
+
+const moves3 = [
+  {p: 'G', x: 0, y: 0},
+  {p: 'Y', x: 0, y: 1},
+  {p: 'G', x: 1, y: 1},
+  {p: 'Y', x: 1, y: 2},
+  {p: 'G', x: 2, y: 2},
+  {p: 'Y', x: 2, y: 3},
+  {p: 'G', x: -1, y: -1},
+  {p: 'Y', x: 3, y: 3}
+];
+
+const moves4 = [
+  {p: 'A', x: 50, y: -50},
+  {p: 'B', x: 49, y: -50},
+  {p: 'C', x: 48, y: -50},
+  {p: 'D', x: 50, y: -49},
+  {p: 'A', x: 51, y: -50},
+  {p: 'B', x: 49, y: -49},
+  {p: 'C', x: 48, y: -51},
+  {p: 'D', x: 51, y: -49}
+]
+
+const moves5 = [
+  {p: 'P', x: 0, y: 1},
+  {p: 'P', x: 0, y: -1},
+  {p: 'P', x: 0, y: -2},
+  {p: 'P', x: 0, y: -3},
+  {p: 'E', x: 0, y: 0},
+  {p: 'E', x: 1, y: -1},
+  {p: 'E', x: 2, y: -2},
+  {p: 'E', x: 3, y: -3}
+]
+
+describe("Tests", function() {
+  it("4 in a row", function() {
+    Test.assertDeepEquals(connectFour(moves1), 'R', 'Should check 4 in a row');
+  });
+  it("4 in a column", function(){
+    Test.assertDeepEquals(connectFour(moves2), 'Y', 'Should check 4 in a column')
+  });
+  it("4 in diagonal", function(){
+    Test.assertDeepEquals(connectFour(moves3), 'G', 'Should check 4 in diagonal')
+  });
+  it("no winner", function(){
+    Test.assertDeepEquals(connectFour(moves4), null, 'Should check if there\'s no winner')
+  });
+  it("4 in diagonal", function(){
+    Test.assertDeepEquals(connectFour(moves5), 'E', 'Should check 4 in diagonal')
+  });
+});
